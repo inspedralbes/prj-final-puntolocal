af582407e2e22fbfea063ad606d571e9170715d9
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-01-21 10:17:18 +0100

    Docker + Laravel + Nuxt

--- front/nuxt/nuxt.config.ts
+++ front/nuxt/nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+  // devServer: {
+  //   host: '0.0.0.0', // Asegura que escuche todas las interfaces
+  //   port: 3000, // Cambia este puerto si lo necesitas
+  // },
 })
