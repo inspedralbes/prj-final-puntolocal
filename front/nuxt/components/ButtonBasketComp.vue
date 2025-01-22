77ac01412d6256fae90dbe86d7dd9a68e6bfdf8e
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2025-01-22 17:01:13 +0100

    info-producto

---
+++ front/nuxt/components/ButtonBasketComp.vue
@@ -0,0 +1,44 @@
+<template>
+    <button @click="addToBasket">
+        <img :src="shoppingBasketIcon" alt="Añadir al carrito" class="svg-icon" />
+    </button>
+</template>
+
+<script>
+import shoppingBasketIcon from '../assets/shopping-basket.svg';
+
+export default {
+    data() {
+        return {
+            shoppingBasketIcon,
+        };
+    },
+    methods: {
+        addToBasket() {
+            console.log("Producto añadido al carrito");
+        },
+    },
+};
+</script>
+
+<style scoped>
+    button {
+        padding: 10px 40px;
+        background-color: #007bff;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    button:hover {
+        background-color: #0056b3;
+    }
+
+    .svg-icon {
+        width: 30px;
+        height: 30px;
+    }
+</style>
