c251eb41dcbe093514754c3459399f1fe72b86fe
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-01-28 10:28:12 +0100

    Fix login

--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+    use App\Http\Controllers\Auth\AuthController;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\OrderController;
@@ -6,7 +7,7 @@
     use App\Http\Controllers\ProductoController;
     use App\Http\Controllers\ComercioController;
     use App\Http\Controllers\SubcategoriaController;
-    use App\Http\Controllers\CategoriaGeneralController;
+    use App\Http\Controllers\CategoriaController;
     Route::get('/user', function (Request $request) {
         return $request->user();
     })->middleware('auth:sanctum');
