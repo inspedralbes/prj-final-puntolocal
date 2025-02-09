91fb73f405a31895963361d238549a7116ec2a68
arnaubarrerosorribas <131618497+arnaubarrero@users.noreply.github.com>
2025-02-09 18:28:54 +0100

    pagina index

---
+++ front/nuxt/pages/categorias/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p @click="todasLasCategorias()">Ver todas las categorías</p>
+    </div>
+</template>
+
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    function todasLasCategorias() {
+        router.push(`/categorias`);
+    }
+</script>
