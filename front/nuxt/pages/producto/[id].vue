b64d2775ac136dea7a16bf50c45ff6a770c470be
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-02-15 13:58:47 +0100

    Canvi Seeder + mini canvi disseny producto vista + quitar login facebook

--- front/nuxt/pages/producto/[id].vue
+++ front/nuxt/pages/producto/[id].vue
@@ -27,12 +27,12 @@
             <div id="corazon" @click="actualizaFavoritos(producto.id)"
                 class="w-16 h-8 flex items-center justify-between cursor-pointer">
                 <svg v-if="authStore?.favoritos?.has(producto?.id)" width="1.5em" height="1.5em" viewBox="0 0 24 24"
-                    fill="#ea4823" stroke="#ea4823" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    fill="#ea4823" stroke="#ea4823" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                     <path
                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
                     </path>
                 </svg>
-                <svg v-else width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="3"
+                <svg v-else width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="1"
                     stroke-linecap="round" stroke-linejoin="round">
                     <path
                         d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
@@ -53,7 +53,7 @@
             </div>
         </div>
 
-        <div class="bg-white rounded-b-md mb-4 mt-20">
+        <div class="bg-white rounded-b-md mb-4 mt-[65px]">
             <div id="imgs" class="h-[400px] w-full max-w-[900px] mx-auto overflow-hidden">
                 <img :src="producto?.imagen ? `${baseUrl}/storage/${producto.imagen}` : `${baseUrl}/storage/productos/default-image.webp`"
                     alt="Imagen del producto" class="h-full w-full object-contain" />
