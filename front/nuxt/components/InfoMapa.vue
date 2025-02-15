30537ca824778abe4ad15a600c66de87413a3e6e
arnaubarrerosorribas <131618497+arnaubarrero@users.noreply.github.com>
2025-02-15 22:34:53 +0100

    Info del local amb estetica

--- front/nuxt/components/InfoMapa.vue
+++ front/nuxt/components/InfoMapa.vue
@@ -1,31 +1,41 @@
 <template>
     <div v-if="show"
         class="fixed top-3/4 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 p-6 shadow-lg z-50 w-[90%] rounded-xl">
-        <div class="flex flex-col items-center w-full max-w-xs">
-            <h3>{{ info.name }}</h3>
+        
+        <div class="flex items-center gap-4 w-full">
+            <!-- Contenedor de la imagen -->
+            <div id="imagenLocal" class="h-[70px] w-[70px] border-2 border-gray-500 border-solid">
+                <!-- Imagen aquí -->
+            </div>
+            
+            <!-- Contenedor de la información -->
+            <div class="flex flex-col items-center w-full max-w-xs" id="infoLocal">
+                <h3>{{ info.name }}</h3>
 
-            <p v-if="info.puntaje_medio" class="text-m text-gray-600"> ⭐ {{ info.puntaje_medio }} / 5</p>
+                <p v-if="info.puntaje_medio" class="text-m text-gray-600 text-right"> ⭐ {{ info.puntaje_medio }} / 5</p>
 
-            <div class="flex w-full items-center text-center gap-5">
+                <div class="flex w-full items-center text-center gap-5">
+                    <p class="w-1/2 text-sm font-bold py-2 text-right gap-10"
+                        :class="estadoLocal === 'Abierto' ? 'text-green-600' : 'text-red-600'">
+                        {{ estadoLocal }}
+                    </p>
 
-                <p class="w-1/2 text-sm font-bold py-2 text-right gap-10"
-                    :class="estadoLocal === 'Abierto' ? 'text-green-600' : 'text-red-600'">
-                    {{ estadoLocal }}
-                </p>
+                    <p class="w-1/2 text-sm text-gray-600 py-2 border-gray-300 text-left">
+                        {{ horarioDelDia }}
+                    </p>
+                </div>
 
-
-                <p class="w-1/2 text-sm text-gray-600 py-2 border-gray-300 text-left">
-                    {{ horarioDelDia }}
-                </p>
+                <button @click="cerrar" id="cerrar">x</button>
+                <button @click="infoDelComercio" 
+                    class="mt-4 px-6 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-200">
+                    Ir al local
+                </button>
 
             </div>
-
-
-            <button @click="cerrar" id="cerrar">x</button>
-            <button @click="infoDelComercio">Ir al local</button>
         </div>
     </div>
 </template>
+
 
 <script setup>
 import { ref, defineProps, defineEmits, computed } from 'vue';
--- front/nuxt/components/MapComponent.vue
+++ front/nuxt/components/MapComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <div @click="goBack" class="text-xl text-gray-700 dark:text-gray-300 cursor-pointer" id="retrocederBtn">
+        <!-- Botón Retroceder (GoBack) visible solo cuando showPopup es falso -->
+        <div v-if="!showPopup" @click="goBack" class="text-xl text-gray-700 dark:text-gray-300 cursor-pointer"
+            id="retrocederBtn">
             <svg width="1.5em" height="1.5em" viewBox="0 0 1024 1024" class="icon" xmlns="http://www.w3.org/2000/svg"
                 fill="#000000">
                 <g id="SVGRepo_iconCarrier">
@@ -12,6 +14,7 @@
             </svg>
         </div>
 
+        <!-- Buscador -->
         <div id="divBuscador">
             <input v-model="codigoPostal" @keyup.enter="buscarPorCodigoPostal" placeholder="Código Postal" />
             <button @click="buscarPorCodigoPostal" id="buscar">
@@ -19,72 +22,75 @@
             </button>
         </div>
 
+        <!-- Contenedor del mapa visible siempre -->
         <div ref="mapContainer" class="map-container"></div>
 
-        <div>
+        <!-- Botón GetLocation visible solo cuando showPopup es falso -->
+        <div v-if="!showPopup">
             <button @click="getLocation" id="btnLocation">
                 <img src="../assets/location.svg" alt="Location">
             </button>
         </div>
 
-        <InfoMapa v-if="showPopup" :info="puebloSeleccionado" @cerrarPopup="cerrarPopup" />
+        <!-- Componente InfoMapa visible solo cuando showPopup es verdadero -->
+        <InfoMapa v-if="showPopup" :info="puebloSeleccionado" @cerrarPopup="cerrarPopup" id="infoMapa" />
     </div>
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue';
-    import 'ol/ol.css';
-    import Map from 'ol/Map';
-    import View from 'ol/View';
-    import TileLayer from 'ol/layer/Tile';
-    import OSM from 'ol/source/OSM';
-    import { fromLonLat } from 'ol/proj';
-    import VectorLayer from 'ol/layer/Vector';
-    import VectorSource from 'ol/source/Vector';
-    import Feature from 'ol/Feature';
-    import Point from 'ol/geom/Point';
-    import Style from 'ol/style/Style';
-    import CircleStyle from 'ol/style/Circle';
-    import Fill from 'ol/style/Fill';
-    import Stroke from 'ol/style/Stroke';
-    import Select from 'ol/interaction/Select';
-    import { click } from 'ol/events/condition';
-    import InfoMapa from './InfoMapa.vue';
-    import { useRoute, useRouter } from "vue-router";
-    import { defaults as defaultControls } from 'ol/control';
+import { onMounted, ref } from 'vue';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import Style from 'ol/style/Style';
+import CircleStyle from 'ol/style/Circle';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import Select from 'ol/interaction/Select';
+import { click } from 'ol/events/condition';
+import InfoMapa from './InfoMapa.vue';
+import { useRoute, useRouter } from "vue-router";
+import { defaults as defaultControls } from 'ol/control';
 
-    const map = ref(null);
-    const router = useRouter();
-    const location = ref(null);
-    const codigoPostal = ref("");
-    const showPopup = ref(false);
-    const mapContainer = ref(null);
-    const puebloSeleccionado = ref({});
-    const vectorSource = ref(new VectorSource());
-    const { $communicationManager } = useNuxtApp();
-    const API_URL = "https://nominatim.openstreetmap.org/search";
+const map = ref(null);
+const router = useRouter();
+const location = ref(null);
+const codigoPostal = ref("");
+const showPopup = ref(false);
+const mapContainer = ref(null);
+const puebloSeleccionado = ref({});
+const vectorSource = ref(new VectorSource());
+const { $communicationManager } = useNuxtApp();
+const API_URL = "https://nominatim.openstreetmap.org/search";
 
-    onMounted(async () => {
-        const vectorLayer = new VectorLayer({ source: vectorSource.value });
+onMounted(async () => {
+    const vectorLayer = new VectorLayer({ source: vectorSource.value });
 
-        map.value = new Map({
-            target: mapContainer.value,
-            layers: [
-                new TileLayer({ source: new OSM() }),
-                vectorLayer
-            ],
-            view: new View({
-                center: fromLonLat([2.15899, 41.38879]),
-                zoom: 10
-            }),
-            controls: defaultControls({ zoom: false })
-        });
+    map.value = new Map({
+        target: mapContainer.value,
+        layers: [
+            new TileLayer({ source: new OSM() }),
+            vectorLayer
+        ],
+        view: new View({
+            center: fromLonLat([2.15899, 41.38879]),
+            zoom: 10
+        }),
+        controls: defaultControls({ zoom: false })
+    });
 
-        await agregarMarcadoresDesdeResponse();
+    await agregarMarcadoresDesdeResponse();
 
-        const selectClick = new Select({ condition: click });
-        map.value.addInteraction(selectClick);
-        selectClick.on('select', (e) => {
+    const selectClick = new Select({ condition: click });
+    map.value.addInteraction(selectClick);
+    selectClick.on('select', (e) => {
         const selectedFeature = e.selected[0];
         if (selectedFeature) {
             const id = selectedFeature.get('id');
@@ -104,138 +110,138 @@
         }
     });
 
+});
+
+const goBack = () => {
+    router.back();
+};
+
+const agregarMarcadoresDesdeResponse = async () => {
+    try {
+        const response = await $communicationManager.getLocations();
+        console.log("Respuesta de la api: ", response);
+
+        response.forEach(({ id, latitude, longitude, nombre, puntaje_medio, horario }) => {
+            if (!isNaN(latitude) && !isNaN(longitude)) {
+                const horarioParseado = JSON.parse(horario);
+                console.log(horarioParseado)
+
+                const marker = new Feature({
+                    geometry: new Point(fromLonLat([longitude, latitude])),
+                    id: id,
+                    name: nombre,
+                    puntaje_medio: puntaje_medio,
+                    horario: horarioParseado
+                });
+
+                marker.setStyle(
+                    new Style({
+                        image: new CircleStyle({
+                            radius: 6,
+                            fill: new Fill({ color: 'blue' }),
+                            stroke: new Stroke({ color: 'white', width: 1 })
+                        })
+                    })
+                );
+
+                vectorSource.value.addFeature(marker);
+            }
+        });
+    } catch (error) {
+        console.error("Error obteniendo ubicaciones:", error);
+    }
+};
+
+
+const buscarPorCodigoPostal = async () => {
+    if (!codigoPostal.value) {
+        alert("Introduce un código postal.");
+        return;
+    }
+
+    try {
+        const response = await fetch(`${API_URL}?postalcode=${codigoPostal.value}&country=ES&format=json`);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const ubicacion = data[0];
+            const lat = parseFloat(ubicacion.lat);
+            const lon = parseFloat(ubicacion.lon);
+            puebloSeleccionado.value = {
+                name: ubicacion.display_name,
+                lat,
+                lon
+            };
+
+            agregarMarcador(lon, lat, puebloSeleccionado.value.name);
+
+            map.value.getView().animate({
+                center: fromLonLat([lon, lat]),
+                zoom: 14,
+                duration: 1000
+            });
+        } else {
+            alert("No se encontró la ubicación.");
+        }
+    } catch (error) {
+        console.error("Error en la búsqueda:", error);
+        alert("Error al buscar el código postal.");
+    }
+};
+
+const agregarMarcador = (lon, lat, name) => {
+    const marker = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+        name: name
     });
 
-    const goBack = () => {
-        router.back();
-    };
 
-    const agregarMarcadoresDesdeResponse = async () => {
-        try {
-            const response = await $communicationManager.getLocations();
-            console.log("Respuesta de la api: ", response);
+    vectorSource.value.addFeature(marker);
+};
 
-            response.forEach(({ id, latitude, longitude, nombre, puntaje_medio, horario }) => {
-                if (!isNaN(latitude) && !isNaN(longitude)) {
-                    const horarioParseado = JSON.parse(horario);
-                    console.log(horarioParseado)
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                location.value = `Lat: ${lat}, Lng: ${lon}`;
 
-                    const marker = new Feature({
-                        geometry: new Point(fromLonLat([longitude, latitude])),
-                        id: id,
-                        name: nombre,
-                        puntaje_medio: puntaje_medio,
-                        horario: horarioParseado 
-                    });
-
-                    marker.setStyle(
-                        new Style({
-                            image: new CircleStyle({
-                                radius: 6,
-                                fill: new Fill({ color: 'blue' }),
-                                stroke: new Stroke({ color: 'white', width: 1 })
-                            })
-                        })
-                    );
-
-                    vectorSource.value.addFeature(marker);
-                }
-            });
-        } catch (error) {
-            console.error("Error obteniendo ubicaciones:", error);
-        }
-    };
-
-
-    const buscarPorCodigoPostal = async () => {
-        if (!codigoPostal.value) {
-            alert("Introduce un código postal.");
-            return;
-        }
-
-        try {
-            const response = await fetch(`${API_URL}?postalcode=${codigoPostal.value}&country=ES&format=json`);
-            const data = await response.json();
-
-            if (data.length > 0) {
-                const ubicacion = data[0];
-                const lat = parseFloat(ubicacion.lat);
-                const lon = parseFloat(ubicacion.lon);
-                puebloSeleccionado.value = {
-                    name: ubicacion.display_name,
-                    lat,
-                    lon
-                };
-
-                agregarMarcador(lon, lat, puebloSeleccionado.value.name);
+                const lonLat = fromLonLat([lon, lat]);
 
                 map.value.getView().animate({
-                    center: fromLonLat([lon, lat]),
-                    zoom: 14,
+                    center: lonLat,
+                    zoom: 16,
                     duration: 1000
                 });
-            } else {
-                alert("No se encontró la ubicación.");
+
+                vectorSource.value.getFeatures().forEach(feature => {
+                    if (feature.get('userLocation')) {
+                        vectorSource.value.removeFeature(feature);
+                    }
+                });
+
+                vectorSource.value.addFeature(userMarker);
+            },
+            (error) => {
+                location.value = `Error: ${error.message}`;
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 0
             }
-        } catch (error) {
-            console.error("Error en la búsqueda:", error);
-            alert("Error al buscar el código postal.");
-        }
-    };
+        );
+    } else {
+        location.value = 'La geolocalización no es compatible con este navegador.';
+    }
+};
 
-    const agregarMarcador = (lon, lat, name) => {
-        const marker = new Feature({
-            geometry: new Point(fromLonLat([lon, lat])),
-            name: name
-        });
-
-
-        vectorSource.value.addFeature(marker);
-    };
-
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const lat = position.coords.latitude;
-                    const lon = position.coords.longitude;
-                    location.value = `Lat: ${lat}, Lng: ${lon}`;
-
-                    const lonLat = fromLonLat([lon, lat]);
-
-                    map.value.getView().animate({
-                        center: lonLat,
-                        zoom: 16,
-                        duration: 1000
-                    });
-
-                    vectorSource.value.getFeatures().forEach(feature => {
-                        if (feature.get('userLocation')) {
-                            vectorSource.value.removeFeature(feature);
-                        }
-                    });
-
-                    vectorSource.value.addFeature(userMarker);
-                },
-                (error) => {
-                    location.value = `Error: ${error.message}`;
-                },
-                {
-                    enableHighAccuracy: true,
-                    timeout: 10000,
-                    maximumAge: 0
-                }
-            );
-        } else {
-            location.value = 'La geolocalización no es compatible con este navegador.';
-        }
-    };
-
-    const cerrarPopup = () => {
-        showPopup.value = false;
-    };
+const cerrarPopup = () => {
+    showPopup.value = false;
+};
 </script>
 
 <style scoped>
-    @import url('../assets/mapa.css');
+@import url('../assets/mapa.css');
 </style>
