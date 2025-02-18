15f293157205ebc260a4c9be9c26e04ae9ee8d44
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2025-02-18 08:28:56 +0100

    camara

---
+++ front/nuxt/pages/cam.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button @click="activarCamara">Abrir Cámara</button>
+        <video ref="video" autoplay v-if="camaraActiva"></video>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            camaraActiva: false,
+        };
+    },
+    methods: {
+        async activarCamara() {
+            try {
+                this.camaraActiva = true;
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                this.$refs.video.srcObject = stream;
+            } catch (error) {
+                console.error("Error al acceder a la cámara:", error);
+            }
+        },
+    },
+};
+</script>
